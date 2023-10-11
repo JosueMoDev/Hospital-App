28af86335ccb3382c35cec6b1246ef29faaa0660
Jonas Morales <josuemodev@gmail.com>
2023-10-10 20:39:33 -0600

    app ts

--- src/app.ts
+++ src/app.ts
@@ -45,18 +45,18 @@ app.use(
 
 //Routes
 
-// app.use("/api/search", require("./routes/searching.route"));
-// app.use("/api/appointments", require("./routes/appointment.route"));
-// app.use("/api/clinics", require("./routes/clinics.route"));
-// app.use("/api/clinic-assignments", require("./routes/clinic-assignment.route"));
-// app.use("/api/file", require("./routes/files.route"));
-// app.use("/api/login", require("./routes/auth.route"));
-// app.use("/api/patient-records", require("./routes/patient-record.route"));
-// app.use("/api/patients", require("./routes/patient.route"));
-// app.use("/api/users", require("./routes/users.route"));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "public/index.html"));
-// });
+app.use("/api/search", require("./routes/searching.route"));
+app.use("/api/appointments", require("./routes/appointment.route"));
+app.use("/api/clinics", require("./routes/clinics.route"));
+app.use("/api/clinic-assignments", require("./routes/clinic-assignment.route"));
+app.use("/api/file", require("./routes/files.route"));
+app.use("/api/login", require("./routes/auth.route"));
+app.use("/api/patient-records", require("./routes/patient-record.route"));
+app.use("/api/patients", require("./routes/patient.route"));
+app.use("/api/users", require("./routes/users.route"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(" server is running on " + process.env.PORT);
