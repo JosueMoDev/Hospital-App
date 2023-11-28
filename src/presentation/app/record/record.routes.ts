640616de606257f3de42a99518b1ea9d2d21002d
Jonas Morales <josuemodev@gmail.com>
2023-11-27 20:29:51 -0600

    route change record status updated

--- src/presentation/app/record/record.routes.ts
+++ src/presentation/app/record/record.routes.ts
@@ -17,7 +17,7 @@ export class RecordRoutes {
         router.patch('/update', controller.updateRecord);
         router.get('/find-one/:id', controller.findOneById);
         router.get('/find-many', controller.findMany);
-        router.get('/hidde/:id', controller.changeStatus);
+        router.patch('/change-status', controller.changeStatus);
 
         return router;
     }
