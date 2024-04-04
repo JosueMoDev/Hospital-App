a1ab7b66cce8f06e27e5995c787cb1190eba2774
Jonas Morales <josuemodev@gmail.com>
2024-04-04 13:46:15 -0600

    cors issue

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,38 +1,51 @@
 import express, { Router } from "express";
-import compression from 'compression';
-import { ExpressFileUploadAdapter } from '../config/adapters/expressFileUploadAdapter';
+import compression from "compression";
+import cors from "cors";
+import { ExpressFileUploadAdapter } from "../config/adapters/expressFileUploadAdapter";
+
+
 interface serverConfig {
-    port: number;
-    routes: Router;
+  port: number;
+  routes: Router;
 }
+
+const corsOptions = {
+  origin: "http://localhost:4200",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true, // Habilita las cookies a través de las solicitudes CORS
+  exposedHeaders: ["Authorization"], // Añade 'Authorization' a los encabezados expuestos
+};
+
 export class Server {
-    public readonly app = express();
-    private serverListener?: any;
-    private readonly port: number;
-    private readonly routes: Router;
+  public readonly app = express();
+  private serverListener?: any;
+  private readonly port: number;
+  private readonly routes: Router;
 
-    constructor(args: serverConfig) {
-        const { port, routes } = args;
+  constructor(args: serverConfig) {
+    const { port, routes } = args;
 
-        this.port = port,
-            this.routes = routes
-    }
+    (this.port = port), (this.routes = routes);
+  }
 
+  async start() {
+    //* Middlewares
+    this.app.use(cors(corsOptions));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true })); //! x-www-form-urlencoded
+    this.app.use(compression());
+    this.app.use(ExpressFileUploadAdapter.configure());
+    //* Routes
+    this.app.use(this.routes);
 
-    async start() {
-        //* Middlewares
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({ extended: true })); //! x-www-form-urlencoded
-        this.app.use(compression());
-        this.app.use(ExpressFileUploadAdapter.configure())
-        //* Routes
-        this.app.use(this.routes);
+    this.serverListener = this.app.listen(this.port, () =>
+      console.log(`Server running on port ${this.port}`)
+    );
+  }
 
-        this.serverListener = this.app.listen(this.port, () => console.log(`Server running on port ${this.port}`));
-
-    }
-
-    public close() {
-        this.serverListener?.close();
-    }
+  public close() {
+    this.serverListener?.close();
+  }
 }
